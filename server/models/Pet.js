4e6e46d9bc60fe5197c179659b1fed4b6d87e4cf
NIkolay Koev <nikslotar@abv.bg>
2022-11-11 23:20:32 +0200

    Remove wrong description requirementes

--- server/models/Pet.js
+++ server/models/Pet.js
@@ -2,13 +2,13 @@ const { Schema, model, Types } = require("mongoose");
 
 
 const petSchema = new Schema({
-    title: { type: String, required: true, },
+    title: { type: String, required: true },
     status: { type: String, required: true },
     location: { type: String, required: true },
     contactName: { type: String, required: true },
     phone: { type: String, required: true },
     imageUrl: { type: String, required: true, default: 'default-profile.png' },
-    description: { type: String, required: true, default: function () { return this.email } },
+    description: { type: String, required: true },
     owner: { type: Types.ObjectId, ref: 'User' },
     likedByUsers: { type: [Types.ObjectId], ref: 'User', default: [] },
     createdAt: { type: String, required: true, default: () => (new Date().toLocaleString('eu-Eu')) },
