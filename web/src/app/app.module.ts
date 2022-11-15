36f15166ecfae9a10a02a5de8cc8cd314ceec9b9
NIkolay Koev <nikslotar@abv.bg>
2022-11-15 23:34:07 +0200

    Setup the frontend and with the main components and style

---
+++ web/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { MainComponent } from './main/main.component';
+import { LatestComponent } from './latest/latest.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    LatestComponent
+  ],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
