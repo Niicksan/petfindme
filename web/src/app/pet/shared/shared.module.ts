55e66a7cbbdc8124ff47eeef778e842e45231db0
NIkolay Koev <nikslotar@abv.bg>
2022-12-09 01:02:03 +0200

    remove doblicate export

--- web/src/app/pet/shared/shared.module.ts
+++ web/src/app/pet/shared/shared.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoaderComponent } from './loader/loader.component';
-import { PetsNotFoundComponent } from './pets-not-found/pets-not-found.component';
-
 
 
 @NgModule({
   declarations: [
-    LoaderComponent,
-    PetsNotFoundComponent
+    LoaderComponent
   ],
   imports: [
     CommonModule
