707f69d882d7ad15a4775150db925fd9ce830dc0
NIkolay Koev <nikslotar@abv.bg>
2022-12-09 01:03:55 +0200

    Comment live db url

--- server/config/config.js
+++ server/config/config.js
@@ -5,6 +5,7 @@ const config = {
         host: 'localhost',
         port: process.env.PORT || 3030,
         dbURL: 'mongodb://localhost:27017/petfindme',
+        //dbURL: 'mongodb+srv://niksan:aiROMoGZxAy7vzJY@petfindme.ocortq9.mongodb.net/?retryWrites=true&w=majority',
         origin: ['http://localhost:3030', 'http://localhost:4200']
     },
     production: {
