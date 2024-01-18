929a1e476741f66822fc52b1d63a194c2d6f6763
Niicksan <niicksana@gmail.com>
2024-01-19 00:08:21 +0200

    Change custom cors middleware configuration

--- server/middlewares/corsMiddleware.js
+++ server/middlewares/corsMiddleware.js
@@ -1,7 +1,10 @@
+const config = require('../config/config');
+
 module.exports = () => (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', config.origin);
     res.setHeader('Access-Control-Allow-Methods', 'HEAD, OPTIONS, GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
 
     next();
 };
