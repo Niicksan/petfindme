6213ba82fbeb6b2e069cc4528d245faf87ff4781
Nikolay Koev <nikolay.koev@lecturio.de>
2024-01-19 10:22:41 +0200

    Change cookie sameSite to 'strict'

--- server/config/session-config.js
+++ server/config/session-config.js
@@ -13,7 +13,7 @@ const sessionConfig = {
         httpOnly: true,
         secure: config.isSessionSecure,
         maxAge: config.sessionMaxAge,
-        sameSite: 'none',
+        sameSite: 'strict',
     },
     store: MongoStore.create({
         mongoUrl: config.dbURL
