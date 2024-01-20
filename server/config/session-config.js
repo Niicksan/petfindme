fe3e99ca9b12253da55952443ce18e168c4d1efd
Niicksan <niicksana@gmail.com>
2024-01-20 19:30:12 +0200

    Add domain to cookie config

--- server/config/session-config.js
+++ server/config/session-config.js
@@ -10,6 +10,7 @@ const sessionConfig = {
     rolling: true, // whether to (re-)set cookie on every response
     saveUninitialized: false, // whether to save empty sessions to the store
     cookie: {
+        domain: config.DOMAIN,
         httpOnly: true,
         secure: config.isSessionSecure,
         maxAge: config.sessionMaxAge,
