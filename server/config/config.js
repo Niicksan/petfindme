c9bfd981b0e03c598db75f555b05a91409316ca8
Niicksan <niicksana@gmail.com>
2024-01-21 12:31:21 +0200

    Remove unused config options

--- server/config/config.js
+++ server/config/config.js
@@ -5,23 +5,19 @@ const config = {
         sessionSecret: 'petfindme',
         isSessionSecure: false,
         sessionMaxAge: 24 * (60 * 60 * 1000), // 1 day of inactivity
-        isSessionProxyEnabled: false,
         host: 'localhost',
         port: process.env.PORT || 3030,
         // dbURL: 'mongodb://localhost:27017/petfindme',
         dbURL: 'mongodb://127.0.0.1:27017/petfindme',
-        domain: 'localhost',
         origin: ['http://localhost:3000']
     },
     production: {
         sessionSecret: process.env.SESSION_SECRET,
         isSessionSecure: !!process.env.IS_SESSION_SECURE,
         sessionMaxAge: parseInt(process.env.SESSION_MAX_AGE),
-        isSessionProxyEnabled: !!process.env.IS_SESSION_PROXY_ENABLED,
         host: process.env.HOST,
         port: parseInt(process.env.PORT),
         dbURL: process.env.DATABASE_URL,
-        domain: process.env.DOMAIN,
         origin: ['https://petfindme.onrender.com']
     }
 };
