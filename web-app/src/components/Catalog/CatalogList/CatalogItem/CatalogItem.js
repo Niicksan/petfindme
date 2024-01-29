41a954f5c02129bf4d5ea1994da8c244bacc0020
Niicksan <niicksana@gmail.com>
2024-01-30 00:28:18 +0200

    Refactor card item

--- web-app/src/components/Catalog/CatalogList/CatalogItem/CatalogItem.js
+++ web-app/src/components/Catalog/CatalogList/CatalogItem/CatalogItem.js
@@ -1,10 +1,10 @@
 import './CatalogItem.scss';
 
 import { Link } from 'react-router-dom';
-import { Card, Box, CardContent, Typography, CardMedia, IconButton } from '@mui/material';
+import { Card, Box, CardContent, Typography, CardMedia, IconButton, Tooltip } from '@mui/material';
 import FavortieBorderIcon from '@mui/icons-material/FavoriteBorder';
-
-// import { useVehicleContext } from '../../../contexts/VehicleContext';
+import FavoriteBorderOutlined from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 export const CatalogItem = ({
     _id,
@@ -16,99 +16,119 @@ export const CatalogItem = ({
 }) => {
     const date = new Date(updatedAt);
     const createdAt = date.toLocaleDateString('Bg-bg', { year: 'numeric', month: 'long', day: 'numeric' });
-
     return (
-        <>
-            <Card className='card' sx={{ boxShadow: 3 }}>
-                <Link to={`/catalog/pet/${_id}`}>
-                    <Box className='card-content-holder'>
-                        <CardMedia
-                            className='image'
-                            component="img"
-                            image={imageUrl}
-                            alt={imageUrl}
-                        />
-                        <CardContent className='content-desc'>
-                            <Typography component="h6">
-                                {title}
-                            </Typography>
-                            <Box className='content-items-holder'>
-                                <Box className='content-items'>
-                                    <Typography color="text.secondary">
-                                        <Typography component='span' className='content-item-left'>Статус: </Typography>{status}
-                                    </Typography>
-                                    <Typography color="text.secondary">
-                                        <Typography component='span' className='content-item-left'>Град: </Typography>{location}
-                                    </Typography>
-                                    <Typography color="text.secondary">
-                                        <Typography component='span' className='content-item-left'>Добавено на: </Typography>{createdAt}
-                                    </Typography>
-                                </Box>
-                            </Box>
-                        </CardContent>
-                    </Box>
-                </Link>
-                <Box className='favorite'>
-                    <IconButton aria-label="favorite">
-                        <FavortieBorderIcon />
-                    </IconButton>
-                </Box>
-            </Card >
-
-            {/* <Card className='card' sx={{ m: 2, width: '80%', maxWidth: '1920px' }}>
-                <CardMedia component='img' to={`/catalog/pet/1`}
-                    sx={{ minWidth: '30%', maxWidth: '40%', flex: 1, objectFit: 'cover' }}
+        <Card sx={{ width: '100%', height: '100%', boxShadow: 3, cursor: 'pointer', borderRadius: '5px' }}>
+            <Box sx={{ position: 'relative', zIndex: 50 }}>
+                <CardMedia
                     className='image'
-                    image="https://www.akc.org/wp-content/uploads/2017/11/Samoyed-standing-in-the-forest.jpg"
-                    title="Изгубено куче"
+                    component="img"
+                    image={imageUrl}
+                    alt={imageUrl}
+                    sx={{
+                        position: 'relative',
+                        height: '200px',
+                        borderRadius: '5px 5px 0 0',
+                    }}
                 />
-                <Box className='card-content-holder'>
-                    <CardContent >
-                        <Typography gutterBottom variant="h5" component="div">
-                            Изгубено куче
-                        </Typography>
-
-                        <Box variant="body2" className='content-items-holder' >
-                            <Box className='content-items' >
-                                <Typography color="text.secondary" >
-                                    <Typography component='span' className='content-item-left'>Статус: </Typography>Изгубено
-                                </Typography>
-                            </Box>
-
-                            <Box className='content-items' >
-                                Изгубен е мъжки самоет на година и половина в района на зала Колодрум в Пловдив.
-                                Много е красив и много умен. Отговаря на името Полар. Много ви моля ако някой го намери да се свърже с мен
-                            </Box>
-
-                            <Box className='content-items' >
-                                <Typography color="text.secondary">
-                                    <Typography component='span' className='content-item-left'>Град: </Typography>Пловдив
-                                </Typography>
-
-                                <Typography color="text.secondary">
-                                    <Typography component='span' className='content-item-left'>За връзка: </Typography>Емилиа
-                                </Typography>
-
-                                <Typography color="text.secondary">
-                                    <Typography component='span' className='content-item-left'>Телефон: </Typography>0888888888
-                                </Typography>
-                            </Box>
-                        </Box>
-                        {isDetails && (
-                            <Box className='content-items'>
-                                <Typography color="text.secondary">
-                                    <Typography component='span' className='content-item-left'>Публикувано на: </Typography>15.12.2022 08:14
-                                </Typography>
-                            </Box>
-                        )}
-                    </CardContent>
-                    <CardActions className='action' sx={{ m: 1, justifyContent: 'flex-end' }}>
-                        <>
-                            <Button component={Link} to={`/catalog/pet/1`} variant="outlined" size="small" >Детайли</Button>
-                        </>
-                    </CardActions>
+                <Tooltip title={<>Харесайте тук</>}>
+                    <FavortieBorderIcon sx={{
+                        position: 'absolute', top: '5px', right: '5px',
+                        color: '#550A21', background: 'rgba(255, 255, 255, 0.3)',
+                        p: '2px', m: '5px', borderRadius: 50, fontSize: '2em'
+                    }} />
+                </Tooltip>
+            </Box>
+            <CardContent sx={{ p: 2, '&:last-child': { pb: 2 }, pt: 0 }}>
+                <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'space-between' }}>
+                    <Typography component="h5" sx={{
+                        pt: 2, pb: 1,
+                        fontSize: '1.1em',
+                        lineHeight: '1.2',
+                        display: '-webkit-box',
+                        overflow: 'hidden',
+                        WebkitBoxOrient: 'vertical',
+                        WebkitLineClamp: 2,
+                        textDecoration: 'none',
+                        color: 'black'
+                    }}>
+                        {title}
+                    </Typography>
                 </Box>
-            </Card > */}
-        </>
+                <Box sx={{ display: 'flex', flexDirection: 'column', }} >
+                    <Typography variant="caption" sx={{ fontSize: '0.9em', lineHeight: '1.5', color: 'dimgray', }}>
+                        <Typography component='span' sx={{ fontSize: '0.9em', fontWeight: 'bolder', lineHeight: '1.5' }} className='content-item-left'>Статус: </Typography> {status}
+                    </Typography>
+                    <Typography variant="caption" sx={{ fontSize: '0.9em', lineHeight: '1.5', color: 'dimgray' }}>
+                        <Typography component='span' sx={{ fontSize: '0.9em', fontWeight: 'bolder', lineHeight: '1.5' }} className='content-item-left'>Град: </Typography> {location}
+                    </Typography>
+                    <Typography variant="caption" sx={{ fontSize: '0.9em', lineHeight: '1.5', color: 'dimgray' }}>
+                        <Typography component='span' sx={{ fontSize: '0.9em', fontWeight: 'bolder', lineHeight: '1.5' }} className='content-item-left'>Добавено на: </Typography> {createdAt}
+                    </Typography>
+                </Box>
+            </CardContent >
+            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
+                <Typography>
+                    Добави в любими
+                </Typography>
+                <Tooltip title={<>Харесайте тук</>}>
+                    <FavortieBorderIcon sx={{
+                        p: '2px', m: '10px', borderRadius: 50, fontSize: '2em',
+                        color: '#550A21', '&:hover': { background: 'rgba(197, 197, 197, 0.5)' }
+                    }} />
+                </Tooltip>
+            </Box>
+        </Card >
     );
 };
+
+// export const CatalogItem = ({
+//     _id,
+//     title,
+//     status,
+//     location,
+//     imageUrl,
+//     updatedAt
+// }) => {
+//     const date = new Date(updatedAt);
+//     const createdAt = date.toLocaleDateString('Bg-bg', { year: 'numeric', month: 'long', day: 'numeric' });
+
+//     return (
+//         <>
+//             <Card className='card' sx={{ boxShadow: 3 }}>
+//                 <Link to={`/catalog/pet/${_id}`}>
+//                     <Box className='card-content-holder'>
+//                         <CardMedia
+//                             className='image'
+//                             component="img3
+//                             image={imageUrl}
+//                             alt={imageUrl}
+//                         />
+//                         <CardContent className='content-desc'>
+//                             <Typography component="h6">
+//                                 {title}
+//                             </Typography>
+//                             <Box className='content-items-holder'>
+//                                 <Box className='content-items'>
+//                                     <Typography color="text.secondary">
+//                                         <Typography component='span' className='content-item-left'>Статус: </Typography>{status}
+//                                     </Typography>
+//                                     <Typography color="text.secondary">
+//                                         <Typography component='span' className='content-item-left'>Град: </Typography>{location}
+//                                     </Typography>
+//                                     <Typography color="text.secondary">
+//                                         <Typography component='span' className='content-item-left'>Добавено на: </Typography>{createdAt}
+//                                     </Typography>
+//                                 </Box>
+//                             </Box>
+//                         </CardContent>
+//                     </Box>
+//                 </Link>
+//                 <Box className='favorite'>
+//                     <IconButton aria-label="favorite">
+//                         <FavortieBorderIcon />
+//                     </IconButton>
+//                 </Box>
+//             </Card >
+//         </>
+//     );
+// };
