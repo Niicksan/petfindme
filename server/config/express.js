6826f98de6cac5e973227e7f53a92f9ac57f5e0d
Niicksan <niicksana@gmail.com>
2024-01-31 18:46:26 +0200

    Revert Proxy

--- server/config/express.js
+++ server/config/express.js
@@ -33,6 +33,10 @@ module.exports = (app) => {
         credentials: true,
     }));
 
+    // trust first proxy
+    app.set('trust proxy', 1)
+
+    // Setup session
     app.use(session(sessionConfig));
 
     app.use(trimBody('password'));
--- server/config/session-config.js
+++ server/config/session-config.js
@@ -18,7 +18,8 @@ const sessionConfig = {
     },
     store: MongoStore.create({
         mongoUrl: config.dbURL
-    })
+    }),
+    proxy: config.isSessionProxyEnabled
 }
 
 module.exports = {
