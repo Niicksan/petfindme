5c30b59af62f4772b66786b3700b32e98d0a3be0
Niicksan <niicksana@gmail.com>
2024-02-05 20:37:22 +0200

    Scroll to top on page change

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -7,10 +7,13 @@ import reportWebVitals from './reportWebVitals';
 
 import App from './App';
 
+import { ScrollToTop } from "./utils/scrollToTop";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     // <React.StrictMode>
     <BrowserRouter>
+        <ScrollToTop />
         <App />
     </BrowserRouter>
     // </React.StrictMode>
