9f9924a1946689af0b97101f0824b6840bbf7278
Niicksan <niicksana@gmail.com>
2024-02-19 22:10:06 +0200

    Remove unused code

--- web-app/src/guards/OwnerGuard.js
+++ web-app/src/guards/OwnerGuard.js
@@ -1,15 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useParams, Outlet, Navigate } from 'react-router-dom';
 
 import { usePetContext } from "../contexts/PetContext";
 import { petServiceFactory } from '../services/petService';
 
 export const OwnerGuard = ({ children }) => {
-    const pathname = window.location.pathname;
-
     const { id } = useParams();
     const { isOwner, setIsOwner } = usePetContext();
-    // const [isOwner, setIsOwner] = useState();
     const petService = petServiceFactory();
 
     useEffect(() => {
