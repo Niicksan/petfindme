04177fb3bb0cefe81881d60e6d63d8431d6c2148
Niicksan <niicksana@gmail.com>
2024-03-04 21:32:40 +0200

    Enable StrictMode

--- web-app/src/index.js
+++ web-app/src/index.js
@@ -11,12 +11,12 @@ import { ScrollToTop } from "./utils/scrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    // <React.StrictMode>
-    <BrowserRouter>
-        <ScrollToTop />
-        <App />
-    </BrowserRouter>
-    // </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <ScrollToTop />
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
